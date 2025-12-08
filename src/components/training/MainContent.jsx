72fdd21f38fd4fa02b5ccfd22996849406703cae
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-08 14:54:22 +0900

    Feat/#6/하단 결과 보기 버튼 생성

---
+++ src/components/training/MainContent.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Title from '../common/Title'
+import Subtitle from '../common/Subtitle'
+import BigArticleSection from './BigArticleSection'
+import SmallArticleSection from './SmallArticleSection'
+import ResultButton from './ResultButton'
+
+const MainContent = () => {
+  return (
+    <div className='flex-1 bg-[#F6F8F6] py-[30px] pl-[50px] overflow-x-hidden'>
+      <div className='flex flex-col gap-2.5'>
+        <div className='flex flex-col gap-10'>
+          <div>
+            <Title text={'오늘의 추천 신문 기사'} />
+            <Subtitle text={'AI가 회원님의 관심사와 인지 능력에 맞춰 추천하는 기사입니다.'} />
+          </div>
+          <BigArticleSection />
+          <SmallArticleSection />
+          <ResultButton />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainContent
