f8dd7122f86aa2c6057739f8170fa4a6aa983bcd
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-08 13:32:56 +0900

    Feat/#6/상단 제목, 소제목 공통 컴포넌트로 생성

---
+++ src/components/common/Subtitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Subtitle({ text }) {
+  return (
+    <>
+      <p className='font-normal text-[18px]'>{text}</p>
+    </>
+  )
+}
+
+export default Subtitle
---
+++ src/components/common/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title({ text }) {
+  return (
+    <>
+      <p className='font-semibold text-[40px]'>{text}</p>
+    </>
+  )
+}
+
+export default Title
