981102d31602ac91eca6bf2a1ede0c81d77c9a46
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-08 14:46:39 +0900

    Chore/#6/lineclamp 플러그인 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#34C9B0', // 서비스 대표색
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
 }
