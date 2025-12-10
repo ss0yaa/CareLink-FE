402d8725d08e0c2772118f91fc94d227c95124bd
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-10 14:32:02 +0900

    Feat/#8/공통 피드백 박스 완성

---
+++ src/components/training/FeedbackBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import CircleCheck from '@/assets/icons/icon-circle-check.svg'
+
+function FeedbackBox({ title, text, boxtitle, feedbackType }) {
+  return (
+    <div className='flex flex-col gap-5 p-[30px] mt-[50px] bg-[#D4ECDB] rounded-[20px]'>
+      <div className='flex flex-row gap-[11px]'>
+        <img src={CircleCheck} />
+        <p className='font-semibold text-[23px]'>{title}</p>
+      </div>
+      <p className='text-[18px]'>{text}</p>
+      <div className='py-[18px] px-[21px] bg-white rounded-[10px]'>
+        <p className='font-semibold text-[20px]'>{boxtitle}</p>
+        <div className='pt-2.5'>
+          {feedbackType === 1 && '첫 번째 피드백 내용입니다.'}
+          {feedbackType === 2 && '두 번째 피드백 내용입니다.'}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeedbackBox
--- src/components/training/SummaryOneLineBox.jsx
+++ src/components/training/SummaryOneLineBox.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SubmitButton from './SubmitButton'
+import FeedbackBox from './FeedbackBox'
 import IconNumber from '@/assets/icons/icon-number-2.svg'
 
 const SummaryOneLineBox = () => {
+  const [showFeedback, setShowFeedback] = useState(false)
+  const handleSubmit = () => {
+    setShowFeedback(true)
+  }
   return (
     <div>
       <div className='flex flex-row items-center gap-[13px]'>
         <img src={IconNumber} />
         <p className='font-medium text-[25px]'>한 줄 요약하기</p>
       </div>
+      {/* 요약 input box */}
+      {/* 제출 버튼 */}
+      <div className='flex justify-center'>
+        <SubmitButton label='최종 피드백 보기' onClick={handleSubmit} />
+      </div>
+      {/* 피드백 박스 */}
+      {showFeedback && (
+        <FeedbackBox
+          title='최종 피드백'
+          text='AI의 자동 요약을 참교해 비교해보세요. 꾸준한 훈련은 인지 능력 향상에 큰 도움이 됩니다.'
+          boxtitle='AI 자동 요약 (참고용)'
+          feedbackType={2}
+        />
+      )}
     </div>
   )
 }
