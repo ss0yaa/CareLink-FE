cbe9abc81f8e92ecd7ec1c90dbefcd17ec208ddd
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-15 23:48:05 +0900

    Feat/#16/타임피커 제외 MedicineCard 컴포넌트 제작

--- src/components/settings/MainEdit.jsx
+++ src/components/settings/MainEdit.jsx
@@ -1,14 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from '../common/Title'
 import Subtitle from '../common/Subtitle'
+import MedicineCard from './MedicineCard'
 
-function MainEdit(props) {
+function MainEdit() {
+  const [medicines, setMedicines] = useState([])
+  const [editingId, setEditingId] = useState(null)
+
+  // 1. 약 정보 조회
+  useEffect(() => {
+    setMedicines([])
+  }, [])
+
+  // 2. 약 추가
+  const handleAdd = () => {
+    const newItem = {
+      id: Date.now(),
+      name: '',
+      time: '',
+    }
+
+    setMedicines((prev) => [...prev, newItem])
+    setEditingId(newItem.id)
+  }
+
+  // 3. 약 정보 저장
+  const handleSave = (id, updatedData) => {
+    setMedicines((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, ...updatedData } : item)),
+    )
+    setEditingId(null)
+  }
+
+  // 4. 약 정보 수정
+  const handleEdit = (id) => {
+    setEditingId(id)
+  }
+
+  // 5. 약 정보 삭제
+  const handleDelete = (id) => {
+    setMedicines((prev) => prev.filter((item) => item.id !== id))
+
+    if (editingId === id) {
+      setEditingId(null)
+    }
+  }
+
   return (
     <div className='flex-1 bg-background py-[30px] px-[50px] overflow-x-hidden'>
       <div className='flex flex-col '>
         <div className='flex flex-col gap-2.5'>
           <Title text={'약 복용 정보 수정'} />
           <Subtitle text={'복용 중인 약의 종류나 복용 시간을 변경할 수 있습니다.'} />
+        </div>
+        {/* 약 리스트 */}
+        <div className='flex flex-col items-center justify-center gap-10 mt-[30px]'>
+          {medicines.map((item) => (
+            <MedicineCard
+              key={item.id}
+              item={item}
+              isEditing={editingId === item.id}
+              onSave={handleSave}
+              onEdit={handleEdit}
+              onDelete={handleDelete}
+            />
+          ))}
+        </div>
+        {/* 항상 하단에 약 추가 버튼 */}
+        <div className='flex flex-col items-center justify-center'>
+          <button
+            onClick={handleAdd}
+            type='button'
+            className='w-[654px] mt-10 py-[21px] font-semibold text-[23px] text-white bg-[#88D4C6] rounded-[10px] hover:bg-primary cursor-pointer'
+          >
+            약 추가
+          </button>
         </div>
       </div>
     </div>
---
+++ src/components/settings/MedicineCard.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react'
+import IconSelect from '@/assets/icons/icon-select.svg'
+import IconEdit from '@/assets/icons/icon-edit.svg'
+import IconTrash from '@/assets/icons/icon-trash.svg'
+
+function MedicineCard({ item, isEditing, onSave, onEdit, onDelete }) {
+  const [name, setName] = useState(item.name)
+  const [time, setTime] = useState(item.time)
+
+  useEffect(() => {
+    if (isEditing) {
+      setName(item.name)
+      setTime(item.time)
+    }
+  }, [isEditing, item])
+  // 시간 선택 모달
+  const handleModal = () => {}
+  return (
+    <div className='w-[700px] relative mx-auto'>
+      <div className=' flex flex-col justify-center px-[150px] py-[45px] bg-white shadow-[0px_2px_2px_3px_rgba(0,0,0,0.25)] rounded-[25px]'>
+        {isEditing ? (
+          <>
+            <div className='flex flex-col gap-2.5'>
+              <p className='font-medium text-[23px]'>약 이름</p>
+              <input
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder='복용하는 약을 입력해주세요.'
+                className='px-[25px] py-[17px] font-semibold text-[18px] bg-white rounded-[10px] border-[1.50px] border-[#B3B3B3]'
+              />
+            </div>
+            <div className='flex flex-col gap-2.5 mt-[30px]'>
+              <p className='font-medium text-[23px]'>복용 시간</p>
+              <button
+                type='button'
+                onClick={handleModal}
+                className='flex flex-row justify-between px-[25px] py-[17px] font-semibold text-[18px] bg-white rounded-[10px] border-[1.50px] border-[#B3B3B3] text-[#B3B3B3] cursor-pointer'
+              >
+                <p>시간을 선택해주세요.</p>
+                <img src={IconSelect} />
+              </button>
+            </div>
+            <button
+              type='button'
+              className='w-fit mt-5 px-[23px] py-[9px] font-semibold text-[18px] text-white bg-[#88D4C6] hover:bg-primary rounded-[10px] cursor-pointer'
+            >
+              시간 추가
+            </button>
+            <button
+              type='button'
+              onClick={() => onSave(item.id, { name, time })}
+              className='mt-[50px] py-[11px] font-semibold text-[23px] text-white bg-primary rounded-[10px] cursor-pointer'
+            >
+              저장하기
+            </button>
+          </>
+        ) : (
+          <>
+            <div className='flex flex-col gap-2.5'>
+              <p className='font-medium text-[23px]'>약 이름</p>
+              <div className='px-[25px] py-[17px] font-semibold text-[18px] bg-white rounded-[10px] border-[1.50px] border-[#B3B3B3]'>
+                {item.name}
+              </div>
+            </div>
+            <div className='flex flex-col gap-2.5 mt-[30px]'>
+              <p className='font-medium text-[23px]'>복용시간</p>
+              <div className='px-[25px] py-[17px] font-semibold text-[18px] bg-white rounded-[10px] border-[1.50px] border-[#B3B3B3]'>
+                {item.time}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+      {!isEditing && (
+        <div className='absolute top-[30px] -right-20 flex flex-col gap-[30px] '>
+          <button onClick={() => onEdit(item.id)} className='cursor-pointer'>
+            <img src={IconEdit} />
+          </button>
+          <button onClick={() => onDelete(item.id)} className='cursor-pointer'>
+            <img src={IconTrash} />
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MedicineCard
