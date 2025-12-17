bf197285d87c42cce80941a8d2fdc57ab680b77d
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-12-17 17:15:33 +0900

    chore/#19/textfield 컴포넌트 common 폴더로 이동

--- src/components/login/LoginInput.jsx
+++ src/components/login/LoginInput.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import React from 'react'
-import TextField from './TextField'
+import TextField from '../common/TextField'
 
 const LoginInput = () => {
   const [phoneNumber, setPhoneNumber] = useState('')
