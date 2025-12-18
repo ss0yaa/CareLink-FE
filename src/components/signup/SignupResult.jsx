834533b132a8017c3fc75d70c93a7973a43803db
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-12-18 15:38:50 +0900

    feat/#21/회원가입 완료 페이지 구현

---
+++ src/components/signup/SignupResult.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import check from '@/assets/icons/icon-agree-on.svg'
+
+const SignupResult = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className='shadow-[0_2px_2px_3px_rgba(0,0,0,0.25)] inset-shadow-[0_2px_2px_0_rgba(0,0,0,0.25)] rounded-[25px] px-52 py-24'>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='flex justify-center items-center relative w-[130px] h-[130px]'>
+          <div className='bg-[#65e2cb] w-[116px] aspect-square rounded-[50%] blur-[25px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'></div>
+          <div className='bg-white w-[130px] aspect-square rounded-[50%] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'></div>
+          <img
+            className='w-[74px] aspect-square absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'
+            src={check}
+            alt='완료'
+          />
+        </div>
+        <div className='py-[30px]'>
+          <p className='font-semibold text-[35px] text-black pb-[30px]'>
+            회원가입이 완료되었습니다!
+          </p>
+          <p className='font-normal text-xl text-center'>
+            환영합니다! <br /> 오늘부터 <span className='text-primary font-semibold'>케어링크</span>
+            와 함께 <br /> 매일의 건강을 든든하게 함께하겠습니다.
+          </p>
+        </div>
+        <div>
+          <button
+            type='button'
+            className='mt-[50px] bg-primary rounded-[10px] px-[100px] py-5 font-semibold text-[23px] text-white cursor-pointer'
+            onClick={() => navigate(`/`)}
+          >
+            시작하기
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignupResult
---
+++ src/pages/signup/SignupResultPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupResult from '@/components/signup/SignupResult'
+
+const SignupResultPage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center relative w-full h-dvh overflow-y-auto'>
+      <SignupResult />
+    </div>
+  )
+}
+
+export default SignupResultPage
