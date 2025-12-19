160f052fe6cbfcaacf010efe07aa27826e74f20d
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-12-19 11:58:49 +0900

    feat/#23/컨디션 카드 컴포넌트

--- src/components/dashboard/ConditionCard.jsx
+++ src/components/dashboard/ConditionCard.jsx
@@ -4,7 +4,7 @@ import condition from '@/assets/icons/icon-dashboard-condition.svg'
 const ConditionCard = () => {
   return (
     <div className='bg-[#DFF0EF] rounded-[15px] p-[21px] min-w-40'>
-      <div className='flex justify-between'>
+      <div className='flex justify-between items-center'>
         <p className='font-semibold text-lg text-black'>컨디션</p>
         <div className='px-[5px] py-2.5 bg-[#B6E5E0] rounded-[15px]'>
           <img src={condition} alt='컨디션' className='aspect-square w-[30px]' />
