9b3a7b325909b355baf6644c9083d3c154eec0d1
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-12-20 02:04:21 +0900

    feat/#26/일기 페이지

--- src/pages/diary/DiaryListPage.jsx
+++ src/pages/diary/DiaryListPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SideNavBar from '@/components/common/SideNavBar'
+import DiaryListContent from '@/components/diary/DiaryListContent'
 
 const DiaryListPage = () => {
   return (
     <div className='flex w-full h-dvh'>
       <SideNavBar />
+      <DiaryListContent />
     </div>
   )
 }
