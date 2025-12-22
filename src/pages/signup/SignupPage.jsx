7f57ca3959f50a9cf8dec8e57a2ddd95cf1f8ee8
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-12-22 15:00:05 +0900

    api/#36/회원가입 연동

--- src/pages/signup/SignupPage.jsx
+++ src/pages/signup/SignupPage.jsx
@@ -4,12 +4,16 @@ import Step2Form from '@/components/signup/Step2Form'
 import Step3Form from '@/components/signup/Step3Form'
 import StepActions from '@/components/signup/StepActions'
 import { useSignupForm } from '@/hooks/useSignupForm'
+import { useNavigate } from 'react-router-dom'
+import { signup } from '@/apis/auth'
 
 const SignupPage = () => {
+  const navigate = useNavigate()
   const {
     step,
     form,
     setField,
+    setBirthday,
     setCog,
     toggleInterest,
     toggleAgree,
@@ -19,12 +23,39 @@ const SignupPage = () => {
     check,
     checkId,
   } = useSignupForm()
+
+  const handleFinalSubmit = async () => {
+    try {
+      await signup({
+        name: form.name,
+        password: form.password,
+        phoneNum: form.phoneNum,
+        birthday: form.birthday,
+        cognitiveState: form.cognitiveState,
+        interestedCategory: form.interestedCategory,
+        caregiverName: form.caregiverName,
+        caregiverPhoneNum: form.caregiverPhoneNum,
+        caregiverEmail: form.caregiverEmail,
+      })
+
+      navigate('/signup/result', {
+        state: {
+          phoneNum: form.phoneNum,
+          password: form.password,
+        },
+      })
+    } catch (err) {
+      console.log(err?.response?.data?.message || '회원가입에 실패했습니다.')
+    }
+  }
+
   return (
     <div className='flex flex-col items-center relative w-full h-dvh py-11 overflow-y-auto'>
       {step === 1 && (
         <Step1Form
           form={form}
           setField={setField}
+          setBirthday={setBirthday}
           setCog={setCog}
           toggleInterest={toggleInterest}
           check={check}
@@ -35,7 +66,14 @@ const SignupPage = () => {
       {step === 3 && <Step3Form agreements={form.agreements} toggleAgree={toggleAgree} />}
       <StepActions
         onPrev={prev}
-        onNext={next}
+        onNext={() => {
+          if (!canGoNext) return
+          if (step === 3) {
+            handleFinalSubmit()
+          } else {
+            next()
+          }
+        }}
         prevDisabled={step === 1}
         nextDisabled={!canGoNext}
       />
