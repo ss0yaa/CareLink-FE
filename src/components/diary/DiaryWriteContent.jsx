bcd8cf587c47c36b5445c3777a1b0a9cb23be59f
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-12-22 22:41:42 +0900

    feat/#39/나의 일기 기록 버튼 페이지 연결

--- src/components/diary/DiaryWriteContent.jsx
+++ src/components/diary/DiaryWriteContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import calender from '@/assets/icons/icon-calender.svg'
 import DiaryTitleInput from './DiaryTitleInput'
 import DiaryTool from './DiaryTool'
@@ -6,6 +7,7 @@ import DiaryBtn from './DiaryBtn'
 import diaryRecord from '@/assets/icons/icon-diary-record.svg'
 
 const DiaryWriteContent = () => {
+  const navigate = useNavigate()
   const today = new Date()
   const formattedDate = `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`
 
@@ -27,6 +29,7 @@ const DiaryWriteContent = () => {
         <div className='flex justify-between'>
           <button
             type='button'
+            onClick={() => navigate('/diary/list')}
             className='border-[1.5px] border-primary rounded-[10px] bg-white font-semibold text-[23px] text-black min-w-[282px] py-5 cursor-pointer flex justify-center items-center'
           >
             <img src={diaryRecord} alt='일기 기록' className='w-[21px] h-6 mr-2.5' />
