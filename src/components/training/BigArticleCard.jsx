61a249cedf7fd149b92dcaccf0c329f022ff1b40
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-22 16:50:50 +0900

    Design/#37/기사 썸네일 이미지 크기 고정

--- src/components/training/BigArticleCard.jsx
+++ src/components/training/BigArticleCard.jsx
@@ -3,14 +3,13 @@ import { useParams, useNavigate } from 'react-router-dom'
 
 function BigArticleCard({ newsId, img, title, time }) {
   const navigate = useNavigate()
-  const { newsId } = useParams()
 
   return (
     <div
       onClick={() => navigate(`/training/article/${newsId}`)}
       className='w-80 h-80 flex flex-col gap-2.5 p-5 rounded-[20px] bg-[#E8E8E8] cursor-pointer'
     >
-      <img className='pb-2.5' src={img} />
+      <img className='w-[280px] h-[170px] rounded-[16px] object-cover pb-2.5' src={img} />
       <p className='font-semibold text-[20px] line-clamp-1'>{title}</p>
       <p className='font-normal text-[18px]'>예상 소요 시간: {time}분</p>
     </div>
--- src/components/training/SmallArticleCard.jsx
+++ src/components/training/SmallArticleCard.jsx
@@ -6,9 +6,9 @@ const SmallArticleCard = ({ newsId, img, title, subtitle, time }) => {
   return (
     <div
       onClick={() => navigate(`/training/article/${newsId}`)}
-      className='w-64 h-80 flex flex-col  gap-2.5 py-5 px-[15px] rounded-[20px] bg-[#E8E8E8] cursor-pointer'
+      className='w-64 h-80 flex flex-col  gap-2.5 py-5 px-[15px] rounded-2xl bg-[#E8E8E8] cursor-pointer'
     >
-      <img className='pb-2.5' src={img} />
+      <img className='w-[219px] h-[110px] rounded-2xl object-coverpb-2.5' src={img} />
       <p className='font-semibold text-[20px] pb-[5px] line-clamp-1'>{title}</p>
       <p className='font-normal text-[18px] line-clamp-2'>{subtitle}</p>
       <p className='font-normal text-[18px]'>예상 소요 시간: {time}분</p>
